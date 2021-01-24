6d5b845283380756e1597bca762e4ae054e9429e
chaukhanh211 <chaukhanh211>
2021-01-24 17:15:31 +0700

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,20 +5,34 @@ var heightHeader  = header.clientHeight;
 // Đóng mở khi chọn menu
 menuMobile.onclick = function () {
     var isClose = header.clientHeight === heightHeader;
-    console.log(isClose);
+    //console.log(isClose);
     if(isClose){
         header.style.height = "auto";
-    } else{
+    } else {
         header.style.height = null;
     }
 }
 // Tự động đóng mở khi chọn menu
 var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
-console.log(menuItems);
-
+//console.log(menuItems);
+var isClickParentMenu = false;
 for(var i = 0; i < menuItems.length; i++){
     var menuItem = menuItems[i];
-    menuItem.onclick = function() {
-        header.style.height = null;
+    menuItem.onclick = function(event) {
+        
+        var isParentMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav');
+        //console.log(isParentMenu);
+        if (isClickParentMenu) {
+            console.log("Dong");
+            isClickParentMenu = false;
+
+        } else if (isParentMenu) {
+            console.log("Mo");
+            isClickParentMenu = true;
+            event.preventDefault();
+        }
+         else {
+            header.style.height = null;
+        }
     }
 }
